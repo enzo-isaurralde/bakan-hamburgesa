0a6a153ca4a7c33e7d45b5939e9f6cead9a06999
Enzo Ariel Isaurralde <enzoarielisaurralde@gmail.com>
2026-03-11 09:26:38 -0300

    archivos faltantes

---
+++ src/main/java/Bakan/Sistema/de/Venta/SistemaDeVentaApplication.java
@@ -0,0 +1,13 @@
+package Bakan.Sistema.de.Venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeVentaApplication.class, args);
+	}
+
+}
