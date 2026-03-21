4652cc4296977d77aa7d0904e880a043be99f504
Enzo Ariel Isaurralde <enzoarielisaurralde@gmail.com>
2026-03-21 01:56:43 -0300

    feat: agregar AutenticacionService implementando UserDetailsService

---
+++ src/main/java/Bakan/Sistema/de/Venta/infra/security/AutenticacionService.java
@@ -0,0 +1,19 @@
+package Bakan.Sistema.de.Venta.infra.security;
+
+import Bakan.Sistema.de.Venta.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacionService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return usuarioRepository.findByUsername(username);
+    }
+}
